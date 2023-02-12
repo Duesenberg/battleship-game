c9e907f7ba519d1a7d79b6878a30a48dffbae2dc
Pande <popovski.pande@gmail.com>
2023-02-12 22:04:17 +0100

    Finish up placeShipsScreen.js
    1. Modified placeShipsScreenContent():
      - Added value attributes to the control buttons (ships & direction
        arrows);
      - Added data-row & data-column attributes to all board squares,
        to conveniently correspond to the indexes of the player board matrix.
    2. Added markBoardSquares():
      - The fn goes through all board squares in the DOM as well as the
        player board matrix. Assigns class of 'occupied' if square contains
        a ship.
    3. Added eraseShipsFromBoard():
      - Fn goes through all squares in DOM & removes the class 'occupied'.
        Also calls another method, which clears board matrix of all ships.
    4. Added placeShipsScreenEventListeners():
      - Fn adds event listeners to control buttons (ships & direction arrows)
        and board squares.
      - Ship buttons call a fn which predetermines which ship type is placed
        on board when a square is clicked.
      - Direction buttons call a fn which predetermines the direction of the
        placed ship.
      - Clicking a square calls a Gameboard() method for placing a ship & calls
        markBoardSquares() after this
      - The 'Clear' button calls eraseShipsFromBoard()
      - The 'Finish' button generates the next screen upon which the game
        starts, but does nothing if all ships are not placed on board before.
    5. Added generateGameScreen() dummy function, which will be imported in future.
    
    Still need to test the event listeners if they are working as intended.

--- src/placeShipsScreen.js
+++ src/placeShipsScreen.js
@@ -1,6 +1,11 @@
 import {selectDOMel, selectDOMelAll, insertDOMel, removeAllChildNodes} from "./auxFnsDOM";
+import { clearObject, updateRound, updateScore, 
+  roundOver, determineRoundWinner, gameOver,
+  determineGameWinner, GenerateGameData, setShipType, setShipDirection  }
+  from "./gameLoopAuxFns";
 
-const placeShipsScreen = () => {
+//create the content of the screen
+const placeShipsScreenContent = () => {
   const container = selectDOMel('#container');
 
   removeAllChildNodes(container);//clear the welcome screen
@@ -17,22 +22,190 @@ const placeShipsScreen = () => {
   //ships div content
   const shipsHeader = insertDOMel('p', ships, 'ships-header', 'Ships:');
   const carrier = insertDOMel('button', ships, 'carrier-button', 'Carrier');
+  carrier.setAttribute('value', 'carrier');
+  carrier.classList.add('pressed');
   const battleship = insertDOMel('button', ships, 'battleship-button', 'Battleship');
+  battleship.setAttribute('value', 'battleship');
   const cruiser = insertDOMel('button', ships, 'cruiser-button', 'Cruiser');
+  cruiser.setAttribute('value', 'cruiser');
   const submarine = insertDOMel('button', ships, 'submarine-button', 'Submarine');
+  submarine.setAttribute('value', 'submarine');
   const destroyer = insertDOMel('button', ships, 'destroyer-button', 'Destroyer');
+  destroyer.setAttribute('value', 'destroyer');
   //controls content
   const controlsHeader = insertDOMel('p', controls, 'controls-header', 'Placement direction:');
   const arrowsContainer = insertDOMel('div', controls, 'arrows-container');
   const upArrow = insertDOMel('button', arrowsContainer, 'arrow-up');
+  upArrow.setAttribute('value', 'up');
   const downArrow = insertDOMel('button', arrowsContainer, 'arrow-down');
+  downArrow.setAttribute('value', 'down');
   const leftArrow = insertDOMel('button', arrowsContainer, 'arrow-left');
+  leftArrow.setAttribute('value', 'left');
   const rightArrow = insertDOMel('button', arrowsContainer, 'arrow-right');
-  const finishPlacement =insertDOMel('button', controls, 'finish-button', 'Finish');
+  rightArrow.setAttribute('value', 'right');
+  rightArrow.classList.add('pressed');
+  const finishPlacement = insertDOMel('button', controls, 'finish-button', 'Finish');
+  const eraseShips = insertDOMel('button', controls, 'clear-ships', 'Clear');
   //board content
-  for (let i = 0; i < 100; i++) {
-    const boardSquare = insertDOMel('div', board, 'board-square');
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const boardSquare = insertDOMel('div', board, 'board-square');
+      boardSquare.setAttribute('data-row', i);
+      boardSquare.setAttribute('data-column', j);
+    }
   }
 }
 
-export {placeShipsScreen};
+//add class of occupied to all filled board squares
+const markBoardSquares = () => {
+  const boardSquares = document.querySelectorAll('.board-square');
+
+  for (let square in boardSquares) {
+    let row = parseInt(square.getAttribute('data-row'));
+    let column = parseInt(square.getAttribute('data-column'));
+
+    if (gameData.player1.gameBoard.board[row][column].ship !== null)
+      square.classList.add('occupied');
+  }
+}
+
+//remove the occupied class from the board squares & clear data from board
+const eraseShipsFromBoard = () => {
+  const boardSquares = document.querySelectorAll('.board-square');
+
+  for (let square in boardSquares) {
+    square.classList.remove('occupied');
+  }
+
+  gameData.player1.gameBoard.clearBoard();
+}
+
+//add event listeners to buttons and board squares
+const placeShipsScreenEventListeners = () => {
+  //declaring elements
+  const carrier = document.querySelector('.carrier-button');
+  const battleship = document.querySelector('.battleship-button');
+  const cruiser = document.querySelector('.cruiser-button');
+  const submarine = document.querySelector('.submarine-button');
+  const destroyer = document.querySelector('.destroyer-button');
+  const upArrow = document.querySelector('.arrow-up');
+  const downArrow = document.querySelector('.arrow-down');
+  const leftArrow = document.querySelector('.arrow-left');
+  const rightArrow = document.querySelector('.arrow-right');
+  const boardSquares = document.querySelectorAll('.board-square');
+
+  //listeners for ship buttons
+  carrier.addEventListener('click', () => {
+    setShipType(carrier.value);
+    carrier.classList.add('pressed');
+    battleship.classList.remove('pressed');
+    cruiser.classList.remove('pressed');
+    submarine.classList.remove('pressed');
+    destroyer.classList.remove('pressed');
+  });
+  battleship.addEventListener('click', () => {
+    setShipType(battleship.value);
+    carrier.classList.remove('pressed');
+    battleship.classList.add('pressed');
+    cruiser.classList.remove('pressed');
+    submarine.classList.remove('pressed');
+    destroyer.classList.remove('pressed');
+  });
+  cruiser.addEventListener('click', () => {
+    setShipType(cruiser.value);
+    carrier.classList.remove('pressed');
+    battleship.classList.remove('pressed');
+    cruiser.classList.add('pressed');
+    submarine.classList.remove('pressed');
+    destroyer.classList.remove('pressed');
+  });
+  submarine.addEventListener('click', () => {
+    setShipType(submarine.value);
+    carrier.classList.remove('pressed');
+    battleship.classList.remove('pressed');
+    cruiser.classList.remove('pressed');
+    submarine.classList.add('pressed');
+    destroyer.classList.remove('pressed');
+  });
+  destroyer.addEventListener('click', () => {
+    setShipType(destroyer.value);
+    carrier.classList.remove('pressed');
+    battleship.classList.remove('pressed');
+    cruiser.classList.remove('pressed');
+    submarine.classList.remove('pressed');
+    destroyer.classList.add('pressed');
+  });
+  //listeners for placement direction
+  upArrow.addEventListener('click', () => {
+    setShipDirection(upArrow.value);
+    upArrow.classList.add('pressed');
+    downArrow.classList.remove('pressed');
+    leftArrow.classList.remove('pressed');
+    rightArrow.classList.remove('pressed');
+  });
+  downArrow.addEventListener('click', () => {
+    setShipDirection(downArrow.value);
+    upArrow.classList.remove('pressed');
+    downArrow.classList.add('pressed');
+    leftArrow.classList.remove('pressed');
+    rightArrow.classList.remove('pressed');
+  });
+  leftArrow.addEventListener('click', () => {
+    setShipDirection(leftArrow.value);
+    upArrow.classList.remove('pressed');
+    downArrow.classList.remove('pressed');
+    leftArrow.classList.add('pressed');
+    rightArrow.classList.remove('pressed');
+  });
+  rightArrow.addEventListener('click', () => {
+    setShipDirection(rightArrow.value);
+    upArrow.classList.remove('pressed');
+    downArrow.classList.remove('pressed');
+    leftArrow.classList.remove('pressed');
+    rightArrow.classList.add('pressed');
+  });
+
+  //listeners for board squares
+  for (let square in boardSquares) {
+    square.addEventListener('click', () => {
+      let row = parseInt(square.getAttribute('data-row'));
+      let column = parseInt(square.getAttribute('data-column'));
+      let shipType = gameData.shipPlacement[0];
+      let shipDirection = gameData.shipPlacement[1];
+
+      gameData.player1.gameBoard.placeSelectedShip(row, column, shipType, shipDirection);
+
+      markBoardSquares();
+    });
+  }
+
+  //listener for clear button
+  const eraseShips = document.querySelector('.clear-ships');
+  eraseShips.addEventListener('click', () => {
+    eraseShipsFromBoard();
+  })
+
+  //listener for finish button
+  const finishPlacement = document.querySelector('.finish-button');
+  finishPlacement.addEventListener('click', () => {
+    let shipsArePlaced;
+
+    shipsArePlaced = gameData.player1.gameBoard.shipsAreReady();
+
+    if (shipsArePlaced === true) generateGameScreen();
+  });
+}
+
+//---------------------dummy function while i'm working on gameScreen
+const generateGameScreen = () => {
+  console.log('no hill for a stepper');
+}
+//------------------------------------------------------
+
+const generatePlaceShipsScreen = () => {
+  placeShipsScreenContent();
+  placeShipsScreenEventListeners();
+}
+
+export {placeShipsScreenContent, generatePlaceShipsScreen, placeShipsScreenEventListeners,
+  markBoardSquares, eraseShipsFromBoard};
--- src/placeShipsScreen.test.js
+++ src/placeShipsScreen.test.js
@@ -4,10 +4,47 @@ import { welcomeScreenContent } from "./welcomeScreen";
 describe('place ships screen', () => {
   document.body.innerHTML = `<div id="container"></div>`;
 
+  let boardSquaresString = '';
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      boardSquaresString += `<div class="board-square" data-row="${i}" data-column="${j}"></div>`;
+    }
+  }
+
   it('should generate DOM elements for ship placement screen', () => {
     welcomeScreenContent();
     placeShipsScreenContent();
 
-    expect(document.body.innerHTML).toBe(`<div id="container"><div class="screen"><div class="header"><h1 class="title">Place your Ships</h1></div><div class="ships"><p class="ships-header">Ships:</p><button class="carrier-button">Carrier</button><button class="battleship-button">Battleship</button><button class="cruiser-button">Cruiser</button><button class="submarine-button">Submarine</button><button class="destroyer-button">Destroyer</button></div><div class="controls"><p class="controls-header">Placement direction:</p><div class="arrows-container"><button class="arrow-up"></button><button class="arrow-down"></button><button class="arrow-left"></button><button class="arrow-right"></button></div><button class="finish-button">Finish</button></div><div class="board"><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div><div class="board-square"></div></div></div></div>`);
+    expect(document.body.innerHTML).toBe(
+      '<div id="container">' +
+        '<div class="screen">' +
+          '<div class="header">' +
+            '<h1 class="title">Place your Ships</h1>' +
+          '</div>' +
+          '<div class="ships">' +
+            '<p class="ships-header">Ships:</p>' +
+            '<button class="carrier-button pressed" value="carrier">Carrier</button>' +
+            '<button class="battleship-button" value="battleship">Battleship</button>' +
+            '<button class="cruiser-button" value="cruiser">Cruiser</button>' +
+            '<button class="submarine-button" value="submarine">Submarine</button>' +
+            '<button class="destroyer-button" value="destroyer">Destroyer</button>' +
+          '</div>' +
+          '<div class="controls">' +
+            '<p class="controls-header">Placement direction:</p>' +
+            '<div class="arrows-container">' +
+              '<button class="arrow-up" value="up"></button>' +
+              '<button class="arrow-down" value="down"></button>' +
+              '<button class="arrow-left" value="left"></button>' +
+              '<button class="arrow-right pressed" value="right"></button>' +
+            '</div>' +
+            '<button class="finish-button">Finish</button>' +
+            '<button class="clear-ships">Clear</button>' +
+          '</div>' +
+          '<div class="board">' +
+          boardSquaresString +
+          '</div>' +
+        '</div>' +
+      '</div>')
   });
 });
