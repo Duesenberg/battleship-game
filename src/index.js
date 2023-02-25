1cf532350f322e12d50ed5d701ea2b1da26ed7ac
Pande <popovski.pande@gmail.com>
2023-02-25 13:42:07 +0100

    Remove unnecesarry imports

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 //import js files
-import { insertDOMel, removeAllChildNodes } from "./auxFnsDOM.js";
-import { Ship } from "./ship.js";
-import { Player } from "./player.js";
-import { Gameboard } from "./gameBoard.js";
-import { clearObject, updateRound, updateScore, 
-  roundOver, determineRoundWinner, gameOver,
-  determineGameWinner, GenerateGameData, setShipType, setShipDirection } 
-  from "./gameLoopAuxFns.js";
-import { gameOverScreen } from "./gameOverScreen.js";
-import { gameScreen } from "./gameScreen.js";
-import { placeShipsScreenContent, generatePlaceShipsScreen, 
-  placeShipsScreenEventListeners, markBoardSquares, eraseShipsFromBoard, 
-  togglePressedClass } from "./placeShipsScreen.js";
-import { welcomeScreenContent, generateWelcomeScreen } from "./welcomeScreen.js";
+import { GenerateGameData } 
+  from "./game.js";
+import { generateWelcomeScreen } from "./welcomeScreen.js";
 
 //import styles
 import "./welcomeScreen.css";
 import "./placeShipsScreen.css";
+import "./gameScreen.css";
 
 window.gameData = GenerateGameData();
 
